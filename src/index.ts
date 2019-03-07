3b94c8df95b19e8aba27d84e9d3b491979279355
Dalton DelPiano <delps1001@gmail.com>
2019-03-07 11:39:43 -0500

    fix(app): listen on PORT

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as Discord from 'discord.js';
 import { config } from './config/config';
 const client = new Discord.Client();
+import * as Koa from 'koa';
 
 client.on('ready', () => {
   console.info(`Logged in as ${client.user.tag}`);
@@ -12,4 +13,14 @@ client.on('message', msg => {
   }
 });
 
-client.login(config.token);
+client.login(config.token).then(string => {
+    console.info(string);
+}).catch( error => {
+    console.error(error);
+});
+
+
+const app: Koa = new Koa();
+const server = app.listen(process.env.PORT || 3995);
+
+module.exports = server;
