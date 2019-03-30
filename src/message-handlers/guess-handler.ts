dae40ad962fd5af9ff23617da8417fc5f95438e7
Dalton DelPiano <delps1001@gmail.com>
2019-03-29 20:19:48 -0400

    feat(guess game): add ability to provide multiple names for a correct answer, fix various bugs

--- src/message-handlers/guess-handler.ts
+++ src/message-handlers/guess-handler.ts
@@ -1,31 +1,36 @@
 import { GuessContextHandler } from './guess-context-handler';
+import * as StringSimilarity from 'string-similarity';
 
 export class GuessHandler {
-  private static validSetGuess = '^!setGuess [a-zA-z]+ [a-zA-z]+ [a-zA-z]+$';
-  private static validGuess = '^!guess [a-zA-z]+ [a-zA-z]+ [a-zA-z]+$';
+  private static readonly validSetGuess = '^!setGuess ([a-z\\|]+ ?)+$';
 
   static handleGuess(guess: string): string {
-    if (!this.validateGuess(guess)) {
-      return `Invalid !guess command format, it must be of the form "${
-        this.validGuess
-      }"`;
-    }
-
     if (!GuessContextHandler.correctAnswer) {
       return 'Nothing to guess!';
     }
 
+    if (!this.validateGuess(guess)) {
+      return `Invalid !guess command format, it must be of the form "${this.buildValidGuessRegex()}"`;
+    }
+
     const guessSplit = guess.split(' ');
     const guesses = guessSplit.slice(1, guessSplit.length);
-    console.info(guesses);
+    console.info(`Guessing: ${guesses}`);
     let numCorrect = 0;
+    const alreadyGuessed = [] as string[];
     for (const userGuess of guesses) {
       for (const correctAnswer of GuessContextHandler.correctAnswer) {
-        if (
-          correctAnswer.includes(userGuess) &&
-          userGuess.length > correctAnswer.length * 0.5
-        ) {
-          numCorrect += 1;
+        if (alreadyGuessed.includes(correctAnswer)) {
+          continue;
+        }
+        const possibleAnswers = correctAnswer.split('|');
+        for (const possibleAnswer of possibleAnswers) {
+          if (
+            StringSimilarity.compareTwoStrings(possibleAnswer, userGuess) > 0.7
+          ) {
+            alreadyGuessed.push(correctAnswer);
+            numCorrect += 1;
+          }
         }
       }
     }
@@ -48,7 +53,7 @@ export class GuessHandler {
         this.validSetGuess
       }"`;
     }
-    const splitMessage = messageContent.split(' ');
+    const splitMessage = messageContent.trim().split(' ');
     const splitMessageLower = [];
     for (const guess of splitMessage) {
       splitMessageLower.push(guess.toLowerCase());
@@ -62,12 +67,23 @@ export class GuessHandler {
   }
 
   private static validateGuess(messageContent: string): boolean {
-    const regex = new RegExp(this.validGuess);
+    const regex = new RegExp(this.buildValidGuessRegex());
     return regex.test(messageContent);
   }
 
   private static validateSetGuess(messageContent: string): boolean {
     const regex = new RegExp(this.validSetGuess);
     return regex.test(messageContent);
+  }
+
+  private static buildValidGuessRegex(): string {
+    const numberOfThingsToGuess = (GuessContextHandler.correctAnswer as string[])
+      .length;
+    let validGuess = `^!guess`;
+    for (let x = 0; x < numberOfThingsToGuess; x = x + 1) {
+      validGuess = `${validGuess} [a-z]+`;
+    }
+    validGuess = `${validGuess}$`;
+    return validGuess;
   }
 }
